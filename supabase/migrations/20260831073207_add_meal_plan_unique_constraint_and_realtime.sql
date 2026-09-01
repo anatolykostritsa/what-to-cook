92914d0f117695b9df14ce0847c5306c6fed78ee
Anatoli <anatoli.kastrytsa@innowise.com>
2026-09-01 16:01:15 +0300

    chore: track reconciled meal plan migration

---
+++ supabase/migrations/20260831073207_add_meal_plan_unique_constraint_and_realtime.sql
@@ -0,0 +1,9 @@
+﻿alter table public.meal_plan drop constraint if exists meal_plan_household_date_meal_unique;
+alter table public.meal_plan add constraint meal_plan_household_date_meal_unique
+unique (household_id, planned_date, meal_type);
+
+do $$
+begin
+  alter publication supabase_realtime add table public.meal_plan;
+exception when duplicate_object then null;
+end $$;
